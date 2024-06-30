42eb46456ce4f5e3acf611a2445d3a34e1e207c6
Sahnawaz Shaban <shaanshaban400@gmail.com>
2024-06-30 13:46:01 +0530

    fragments

--- scm/src/main/java/com/scm/controllers/PageController.java
+++ scm/src/main/java/com/scm/controllers/PageController.java
@@ -3,13 +3,15 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(Model model){
-        //Model is object from 'org.springframework.ui.Model'
+    public String home(Model model) {
+        // Model is object from 'org.springframework.ui.Model'
         System.out.println("Home page handler.");
 
         // sending data to view
@@ -17,5 +19,17 @@ public class PageController {
         model.addAttribute("youTube_channel", "ElizaElix");
         model.addAttribute("gitHub_Repo", "https://github.com/SahnawazShaban");
         return "home";
+    }
+
+    @RequestMapping("/about")
+    public String aboutPage() {
+        System.out.println("About Page loading.");
+        return "about";
+    }
+
+    @RequestMapping("/service")
+    public String servicesPage() {
+        System.out.println("Servies page loading.");
+        return "service";
     }
 }
